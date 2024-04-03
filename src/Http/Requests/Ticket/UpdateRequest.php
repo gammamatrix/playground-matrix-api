080fe13ed1ac954cab8654a678f459c401bde7d2
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2024-04-02 18:11:42 -0700

    GH-1

--- src/Http/Requests/Ticket/UpdateRequest.php
+++ src/Http/Requests/Ticket/UpdateRequest.php
@@ -138,6 +138,54 @@ class UpdateRequest extends BaseUpdateRequest
         $this->filterStatus($input);
         $this->filterSystemFields($input);
 
+        if ($this->exists('handler')) {
+            $input['handler'] = $this->filterHtml($this->input('handler'));
+        }
+
+        if ($this->exists('priority')) {
+            $input['priority'] = $this->filterHtml($this->input('priority'));
+        }
+
+        if ($this->exists('severity')) {
+            $input['severity'] = $this->filterHtml($this->input('severity'));
+        }
+
+        if ($this->exists('resolution')) {
+            $input['resolution'] = $this->filterHtml($this->input('resolution'));
+        }
+
+        if ($this->exists('step')) {
+            $input['step'] = $this->filterHtml($this->input('step'));
+        }
+
+        if ($this->exists('state')) {
+            $input['state'] = $this->filterHtml($this->input('state'));
+        }
+
+        if ($this->exists('workflow_type')) {
+            $input['workflow_type'] = $this->filterHtml($this->input('workflow_type'));
+        }
+
+        if ($this->exists('actual')) {
+            $input['actual'] = $this->purify($this->input('actual'));
+        }
+
+        if ($this->exists('expected')) {
+            $input['expected'] = $this->purify($this->input('expected'));
+        }
+
+        if ($this->exists('steps')) {
+            $input['steps'] = $this->purify($this->input('steps'));
+        }
+
+        if ($this->exists('story')) {
+            $input['story'] = $this->purify($this->input('story'));
+        }
+
+        if ($this->exists('criteria')) {
+            $input['criteria'] = $this->purify($this->input('criteria'));
+        }
+
         if (! empty($input)) {
             $this->merge($input);
         }
